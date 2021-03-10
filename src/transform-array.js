76d991eaffa3607002dce4767cc7b7e6291aad76
kingurru <kingnikoriginal@gmail.com>
2021-03-10 23:52:13 +0300

    init: start array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error()
+    }
+
+    let newArr = arr.concat()
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '--double-next') {
+            newArr[i] = newArr[i + 1]
+        } else if (arr[i] === '--double-prev') {
+            newArr[i] = newArr[i - 1]
+        } else if (arr[i] === '--discard-next') {
+            newArr[i] = newArr[i + 1] = undefined
+        } else if (arr[i] === '--discard-prev') {
+            newArr[i] = newArr[i - 1] = undefined
+        }
+    }
+
+    return newArr.filter((item) => item !== undefined)
+
+}
