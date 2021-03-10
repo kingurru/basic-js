bd432763c4151e4b2a1f45dcc95ee10b20e51f34
kingurru <kingnikoriginal@gmail.com>
2021-03-10 20:15:08 +0300

    init: start task repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,19 @@
 const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+    if (!(str || options)) return false
+    let {
+        repeatTimes = 1,
+        separator = '+',
+        addition = '',
+        additionRepeatTimes = 1,
+        additionSeparator = '|',
+    } = options
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+    str = String(str)
+    addition = String(addition)
+
+    let additionsString = new Array(additionRepeatTimes).fill(addition).join(additionSeparator)
+    let fullString = new Array(repeatTimes).fill(str + additionsString).join(separator)
+
+    return fullString
+}
