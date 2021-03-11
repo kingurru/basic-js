ed620d3488074d5727f2cca1c62e70b1572d11a1
kingurru <kingnikoriginal@gmail.com>
2021-03-11 17:31:44 +0300

    fix: fix encrypt

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -8,15 +8,23 @@ class VigenereCipheringMachine {
     encrypt(message, key) {
         if (message === undefined || key === undefined) throw new Error()
         key = key.toUpperCase()
+
         let ab = []
+        let matrix = []
+        let str = ''
+
         for (let i = 65; i < 91; i++) {
             ab.push(String.fromCodePoint(i))
         }
-        console.log('1', message, key)
-        while (message.length > key.length) key += key
-        console.log('2', key)
 
+        for (let i = 0; i < ab.length; i++) {
+            matrix[i] = ab.slice(i).concat(ab.slice(0, i));
+        }
 
+        for (let i = 0; i < message.length; i++) {
+           str += matrix[indexOf(ab.message[i])][indexOf(key[i])]
+        }
+        console.log(str)
     }
 
     decrypt() {
